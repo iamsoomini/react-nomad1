b531eab4cc49e71d6196a0c9d341756dc8100054
soomini <gsssm94@gmail.com>
2024-03-15 10:13:47 +0900

    react basic end

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,15 +2,14 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 
-
 function Detail(){
+    // useParams()는 라우터로 넘겨준 변수를 받아올 수 있음. url의 상세정보를 받아옴.  
     const id  = useParams();
 
     const getMovies = async() => {
         const json = await(
             await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
         ).json();
-        console.log(json);    
     }
 
     useEffect(() => {
